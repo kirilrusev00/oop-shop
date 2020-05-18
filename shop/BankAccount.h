2bc743e56b1129a3a511923e3981626b82ac5840
unknown <kiril.rusev2000@gmail.com>
2020-05-18 17:36:42 +0300

    Replace char* with std::string in class BankAccount

--- shop/BankAccount.h
+++ shop/BankAccount.h
@@ -1,22 +1,19 @@
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
 #include <iostream>
+#include <string>
 
 class BankAccount {
 private:
-	char* bankName;
-	char* iban;
+	std::string bankName;
+	std::string iban;
 
 	std::istream& input(std::istream&);
 
 public:
 	BankAccount();
-	BankAccount(const BankAccount&);
-	BankAccount& operator=(const BankAccount&);
 
 	friend std::istream& operator>>(std::istream&, BankAccount&);
-
-	~BankAccount();
 };
 
 
